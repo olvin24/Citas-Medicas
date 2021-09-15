1e892a240f1c41dc675d3247d2077f5642992a0a
Olvin Romero <olvinromero24@gmail.com>
2021-09-15 14:04:57 -0600

    Agregando programa frase motivadora

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(" Hola Mundo ");
+            MessageBox.Show(" Se tan bueno que no puedan ignorarte. ");
         }
     }
 }
